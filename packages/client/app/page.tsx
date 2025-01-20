805198834f94a8a1da7462fc5b9faa9bc20fbf96
Jahid0178 <zahidul.haque767@gmail.com>
2025-01-21 01:31:30 +0600

    fix: fixed script issue for build in package.json file

--- packages/client/app/page.tsx
+++ packages/client/app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const HomePage = () => {
   return (
     <section>
-      <h1>Home Page</h1>
+      <h1 className="text-5xl font-semibold text-center mt-4">Home Page</h1>
     </section>
   );
 };
